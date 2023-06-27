09e7065cd7d0e8ce2c2f731ff2af148971ba897c
Tureright <sonee@outlook.es>
2023-06-27 08:55:19 -0500

    Inerfaz de registro en las clases Main y ViewInicio

--- src/main/java/epn/edu/ec/Main.java
+++ src/main/java/epn/edu/ec/Main.java
@@ -2,14 +2,20 @@ package epn.edu.ec;
 
 import epn.edu.ec.controller.UsuarioDAO;
 import epn.edu.ec.model.UsuarioEntity;
+import epn.edu.ec.view.ViewInicio;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hola mundo!");
         UsuarioEntity usuario = new UsuarioEntity();
         usuario.setCorreo("cuquita@gmail.com");
         usuario.setPwd("weawea58");
-
+        new UsuarioDAO().crear(usuario);
         System.out.println(new UsuarioDAO().buscarUsuario(usuario));
+
+        System.out.println("----");
+        UsuarioEntity nuevoUsuario = ViewInicio.registro();
+        new UsuarioDAO().crear(nuevoUsuario);
     }
 }
--- src/main/java/epn/edu/ec/view/ViewInicio.java
+++ src/main/java/epn/edu/ec/view/ViewInicio.java
@@ -1,4 +1,25 @@
 package epn.edu.ec.view;
+import epn.edu.ec.model.UsuarioEntity;
 
+import java.util.Scanner;
 public class ViewInicio {
+
+    public static UsuarioEntity registro(){
+        UsuarioEntity usuario = new UsuarioEntity();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Registrar un nuevo usuario");
+        System.out.print("Ingrese su correo electrónico: ");
+        usuario.setCorreo(scanner.nextLine());
+
+        System.out.print("Ingrese su contraseña: ");
+        usuario.setPwd(scanner.nextLine());
+
+        System.out.print("Ingrese su rol: ");
+        usuario.setRol(Integer.parseInt(scanner.nextLine()));
+
+        scanner.close();
+
+        return usuario;
+    }
 }
